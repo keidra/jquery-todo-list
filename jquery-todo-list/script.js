7628e5a118bb00de07daa379553e1a8159062009
keidra <keidrabaldwin@gmail.com>
2016-03-29 23:06:40 -0700

    completed to-do list

--- jquery-todo-list/script.js
+++ jquery-todo-list/script.js
@@ -1,15 +1,18 @@
 $('#myForm').submit(function(event){
-  if ($('input').val() !== '') {
-    var input_value = $('input').val();
+  if ($('input:text').val() !== '') {
+    var input_value = $('input:text').val();
     $('ol').append('<li>' + input_value + '<a href= "">x</a></li>');
   };
 
-  $('input').val('');
+  $('input:text').val('');
   return false
+
+});
 
   $(document).on('click', 'a', function (event) {
     event.preventDefault();
     $(this).parent().remove();
 });
+  
 
-});
+
